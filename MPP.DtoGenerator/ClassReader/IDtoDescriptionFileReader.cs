64dffa7b5cb20205feb26380f4ae1c2ce222b11d
speane <avtolubitel225@gmail.com>
2016-11-06 20:09:48 +0300

    Added Json class file reader

---
+++ MPP.DtoGenerator/ClassReader/IDtoDescriptionFileReader.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using DtoGenerator.DtoClass;
+
+namespace MPP.DtoGenerator.ClassReader
+{
+    public interface IDtoDescriptionFileReader
+    {
+        DtoDescriptionDocument ReadDtoDescription(string filename);
+    }
+}
---
+++ MPP.DtoGenerator/ClassReader/JSONClassFileReader.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using DtoGenerator.DtoClass;
+using Newtonsoft.Json;
+
+namespace MPP.DtoGenerator.ClassReader
+{
+    public class JsonClassFileReader : IDtoDescriptionFileReader
+    {
+        public DtoDescriptionDocument ReadDtoDescription(string filename)
+        {
+            DtoDescriptionDocument document =
+                JsonConvert.DeserializeObject<DtoDescriptionDocument>(File.ReadAllText(filename));
+            
+            return document;
+        }
+    }
+}
