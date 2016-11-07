715f729b41a491b5942213250e2a4d1996f4eb28
speane <avtolubitel225@gmail.com>
2016-11-07 22:12:09 +0300

    Cleared

---
+++ MPP.DtoGenerator/Document/DtoDescriptionDocument.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using DtoGenerator.Dto.Description;
+using Newtonsoft.Json;
+
+namespace DtoGenerator.DtoClass
+{
+    public class DtoDescriptionDocument
+    {
+        [JsonProperty("classDescriptions")]
+        public List<DtoClassDescription> ClassDescriptions { get; set; }
+    }
+}
